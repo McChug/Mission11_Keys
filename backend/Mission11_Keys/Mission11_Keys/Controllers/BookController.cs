4bc41f6665d1d0b3d6cb5d9647dc8a33d4f4f259
McChug <mcchug2112@gmail.com>
2025-03-19 10:10:22 -0600

    first

---
+++ backend/Mission11_Keys/Mission11_Keys/Controllers/BookController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Mission11_Keys.Data;
+
+namespace Mission11_Keys.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private BookContext _bookContext;
+
+        public BookController(BookContext temp)
+        {
+            _bookContext = temp;
+        }
+
+        [HttpGet(Name = "GetBook")]
+        public IEnumerable<Book> Get()
+        {
+            var bookList = _bookContext.Books.ToList();
+
+            return bookList;
+        }
+    }
+}
---
+++ backend/Mission11_Keys/Mission11_Keys/Data/Book.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mission11_Keys.Data
+{
+    public class Book
+    {
+        [Required]
+        public int BookID { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        public string Publisher { get; set; }
+        [Required]
+        public string ISBN { get; set; }
+        [Required]
+        public string Classification { get; set; }
+        [Required]
+        public string Category { get; set; }
+        [Required] 
+        public int pageCount { get; set; }
+        [Required]
+        public int price { get; set; }
+
+    }
+}
